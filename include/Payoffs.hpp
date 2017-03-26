913104c95d7eab234dd5f5af6caabe7669b2fa7d
StanIsAdmin <sgueniff@ulb.ac.be>
2017-03-26 15:47:16 +0200

    Move payoffs to their own file Allows us to include GamePayoffs in Strategies as well as Simulation

---
+++ include/Payoffs.hpp
@@ -0,0 +1,17 @@
+#ifndef PAYOFFS_H
+#define PAYOFFS_H
+
+typedef unsigned short payoff;
+
+/*Contains the payoffs for each possible outcome of a game*/
+struct GamePayoffs {
+	payoff both_cooperate = 0;
+	payoff both_defect = 0;
+	payoff self_cooperates_other_defects = 0;
+	payoff self_defects_other_cooperates = 0;
+	
+	//Assigns payoffs to both players depending on their choices and the game rules
+	void payoffsFromChoices(bool playerACoops, bool playerBCoops, payoff& playerAPayoff, payoff& playerBPayoff) const;
+};
+
+#endif // PAYOFFS_H
