c01ace5fe5cfc6244382620e48706f4240179dac
StanIsAdmin <sgueniff@ulb.ac.be>
2017-04-09 15:09:18 +0200

    Generate strategies repartitions graph

--- out/plotSimulation.m
+++ out/plotSimulation.m
@@ -8,6 +8,7 @@ function plotSimulation(fileName)
   xlabel("Simulation time (generations)")
   ylabel("Average intelligence (inner nodes)")
   print -djpg "Intelligence.jpg"
+  close
   
   #Cooperation
   plot(avg_cooperation)
@@ -15,6 +16,18 @@ function plotSimulation(fileName)
   xlabel("Simulation time (generations)")
   ylabel("Cooperation frequency")
   print -djpg "Cooperation.jpg"
+  close
+  
+  #Strategies
+  stratSum = [count_defect (count_tittat + count_twotat) count_pavlov count_cooper];
+  area(stratSum)
+  title("Repartiton of pure strategies per generation")
+  xlabel("Simulation time (generations)")
+  ylabel("Strategies amongst the population")
+  legend("Always defect", "Tit for tat-like", "Pavlov-like", "Always cooperate", "location", "southoutside", "orientation", "horizontal")
+  legend boxoff
+  print -djpg "Strategies.jpg"
+  close
   
   #Correlation between cooperation and intelligence
   spearman_cooperation_intelligence = spearman(avg_intelligence, avg_cooperation)
