91c3aabeab29f3fdcaace9e56db92737a9174852
StanIsAdmin <sgueniff@ulb.ac.be>
2017-04-21 17:26:12 +0200

    Remove magic numbers from RNG tests

--- include/RngTest.hpp
+++ include/RngTest.hpp
@@ -3,11 +3,22 @@
 
 #include "Rng.hpp"
 
+#include <math.h>
 #include <iostream>
 #include <array>
 #include <cassert>
 
 #define SAMPLE_SIZE 1000
+#define BOOL_GOAL 0.5
+#define BOOL_DIFF 0.1
+#define NUMVAL_GOAL 0
+#define NUMVAL_DIFF 0.1
+#define ITERATIONS_GOAL 50
+#define ITERATIONS_DIFF 5
+#define FITNESS_LOW 0.1
+#define FITNESS_HIGH 0.9
+#define FITNESS_LOW_FACTOR 6
+#define FITNESS_HIGH_FACTOR 12
 
 void testRng();
 
--- src/RngTest.cpp
+++ src/RngTest.cpp
@@ -1,49 +1,48 @@
 #include "RngTest.hpp"
 
+
 void testRng()
 {
 	std::cout << "Testing Rng...";
 	
-	///Random boolean generation
-	int true_count = 0;
-	for (int i=0; i<1000; ++i) {
-		if (RNG::getRandomBool()) true_count++;
+	///Random booleans
+	double true_freq = 0;
+	for (int i=0; i<SAMPLE_SIZE; ++i) {
+		if (RNG::getRandomBool()) true_freq += 1;
 	}
-	assert(abs(true_count - 500) < 50);
+	true_freq /= SAMPLE_SIZE;
+	assert(fabs(true_freq - BOOL_GOAL) < BOOL_DIFF);
 	
-	std::cout << " done!" << std::endl;
-	
-	///Iteration count generation
-	int iterations = 0;
-	for (int i=0; i<1000; ++i) {
-		iterations += RNG::getIterationCount();
+	///Iteration count
+	double avg_iterations = 0;
+	for (int i=0; i<SAMPLE_SIZE; ++i) {
+		avg_iterations += RNG::getIterationCount();
 	}
-	assert(abs(iterations - 50000) < 5000);
+	avg_iterations /= SAMPLE_SIZE;
+	assert(fabs(avg_iterations - ITERATIONS_GOAL) < ITERATIONS_DIFF);
 	
-	///Numval generation
-	double average_numval = 0;
-	for (int i=0; i<1000; ++i) {
-		average_numval += RNG::getRandomNumval();
+	///Numeric values
+	double avg_numval = 0;
+	for (int i=0; i<SAMPLE_SIZE; ++i) {
+		avg_numval += RNG::getRandomNumval();
 	}
-	average_numval /= 1000;
-	if (average_numval < 0) average_numval *= -1;
-	assert(average_numval < 0.1);
+	avg_numval /= SAMPLE_SIZE;
+	assert(fabs(avg_numval - NUMVAL_GOAL) < NUMVAL_DIFF);
 	
 	///Population selection
-	std::array<double, 1000> fitness;
-	for (int i=0; i<1000; ++i) {
-		if (i < 500) fitness[i] = 0.1;
-		else fitness[i] = 0.9;
-	}
-	
-	std::array<int, 1000> new_pop;
-	RNG::selectPopulation(fitness, new_pop);
+	std::array<double, SAMPLE_SIZE> fitness;
+	std::array<int, SAMPLE_SIZE> selected_pop;
 	int lower_pop(0), upper_pop(0);
-	for (int i=0; i<1000; ++i) {
-		if (new_pop[i] > 500) upper_pop++;
+	for (int i=0; i<SAMPLE_SIZE; ++i) {
+		if (i < (SAMPLE_SIZE/2)) fitness[i] = FITNESS_LOW;
+		else fitness[i] = FITNESS_HIGH;
+	}
+	RNG::selectPopulation(fitness, selected_pop);
+	for (int i=0; i<SAMPLE_SIZE; ++i) {
+		if (selected_pop[i] > (SAMPLE_SIZE/2)) upper_pop++;
 		else lower_pop++;
 	}
+	assert(upper_pop > (FITNESS_LOW_FACTOR * lower_pop) and upper_pop < (FITNESS_HIGH_FACTOR * lower_pop));
 	
-	std::cout << upper_pop << ", " << lower_pop << std::endl;
-	assert(upper_pop > (7 * lower_pop) and upper_pop < (11 * lower_pop));
+	std::cout << " done!" << std::endl;
 }
